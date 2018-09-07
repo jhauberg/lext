04bd010c3b85ce5a5ab1a312f9cdebbf95beafe7
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2018-09-07 11:57:42 +0200

    Refactor and cleanup
    This commit reorders the appearance of function implementations, so it
    looks a bit messy and larger than it is.
    
    The most significant and real change is refactoring
    `lxt_resolve_container` into a separate function `lxt_append_entry`
    which is much more indicative of what actually happens. “Resolving a
    container” does not make sense.

--- src/lext.c
+++ src/lext.c
@@ -50,33 +50,20 @@ struct lxt_cursor {
     size_t length;
 };
 
+static uint32_t lxt_rand32(uint32_t * seed);
+
 static int32_t lxt_parse(struct lxt_template *, char const * pattern);
 static char const * lxt_parse_next(struct lxt_token *, enum lxt_kind *, char const * pattern);
+
 static int32_t lxt_resolve_generator(struct lxt_cursor *, struct lxt_generator const *, struct lxt_template const *);
-static int32_t lxt_resolve_variable(struct lxt_cursor *, struct lxt_token const * variable, struct lxt_template const *);
-static int32_t lxt_resolve_container(struct lxt_cursor *, struct lxt_container const *, struct lxt_template const *);
+static int32_t lxt_resolve_variable(struct lxt_cursor *, struct lxt_token const *, struct lxt_template const *);
+
+static int32_t lxt_append_entry(struct lxt_cursor *, struct lxt_token const *);
 
 static struct lxt_generator const * lxt_find_generator(struct lxt_token const *, struct lxt_template const *);
 static struct lxt_container const * lxt_find_container(struct lxt_token const *, struct lxt_template const *);
 
 static bool lxt_token_equals(struct lxt_token const *, char const * name, size_t length);
-
-static uint32_t lxt_rand32(uint32_t * seed);
-
-static
-uint32_t
-lxt_rand32(uint32_t * const seed)
-{
-    uint32_t x = *seed;
-    
-    x ^= x << 13;
-    x ^= (x & 0xffffffffUL) >> 17;
-    x ^= x << 5;
-    
-    *seed = x;
-    
-    return (x & 0xffffffffUL);
-}
 
 void
 lxt_gen(char * const result, size_t const length, char const * const pattern, struct lxt_opts const options)
@@ -143,189 +130,18 @@ lxt_gen(char * const result, size_t const length, char const * const pattern, st
 }
 
 static
-int32_t
-lxt_resolve_container(struct lxt_cursor * const cursor,
-                      struct lxt_container const * const container,
-                      struct lxt_template const * const template)
+uint32_t
+lxt_rand32(uint32_t * const seed)
 {
-    size_t const random_index = lxt_rand32(template->seed) % container->entry_count;
+    uint32_t x = *seed;
     
-    struct lxt_token const * entry = &container->entries[random_index];
+    x ^= x << 13;
+    x ^= (x & 0xffffffffUL) >> 17;
+    x ^= x << 5;
     
-    if (entry->length >= cursor->length - cursor->offset) {
-        return -1;
-    }
+    *seed = x;
     
-    cursor->offset += entry->length;
-    
-    memcpy(cursor->buffer, entry->start, entry->length);
-    
-    return 0;
-}
-
-static
-bool
-lxt_token_equals(struct lxt_token const * const token,
-                  char const * const name,
-                  size_t const length)
-{
-    if (token->length != length) {
-        return false;
-    }
-    
-    if (strncmp(token->start, name, token->length) != 0) {
-        return false;
-    }
-    
-    return true;
-}
-
-static
-struct lxt_generator const *
-lxt_find_generator(struct lxt_token const * const token,
-                    struct lxt_template const * const template)
-{
-    for (size_t i = 0; i < template->generator_count; i++) {
-        struct lxt_generator const * const match = &template->generators[i];
-        
-        if (lxt_token_equals(token, match->entry.start, match->entry.length)) {
-            return match;
-        }
-    }
-    
-    return NULL;
-}
-
-static
-struct lxt_container const *
-lxt_find_container(struct lxt_token const * const token,
-                    struct lxt_template const * const template)
-{
-    for (size_t i = 0; i < template->container_count; i++) {
-        struct lxt_container const * const match = &template->containers[i];
-        
-        if (lxt_token_equals(token, match->entry.start, match->entry.length)) {
-            return match;
-        }
-    }
-    
-    return NULL;
-}
-
-static
-int32_t
-lxt_resolve_variable(struct lxt_cursor * const cursor,
-                     struct lxt_token const * const variable,
-                     struct lxt_template const * const template)
-{
-    struct lxt_cursor tmp;
-    
-    tmp.buffer = cursor->buffer + cursor->offset;
-    tmp.length = cursor->length - cursor->offset;
-    tmp.offset = 0;
-    
-    struct lxt_generator const * const generator = lxt_find_generator(variable,
-                                                                      template);
-    
-    if (generator != NULL) {
-        if (lxt_resolve_generator(&tmp, generator, template) != 0) {
-            return -1;
-        }
-        
-        cursor->offset += tmp.offset;
-        
-        return 0;
-    }
-
-    struct lxt_container const * const container = lxt_find_container(variable,
-                                                                      template);
-    
-    if (container == NULL) {
-        return -1;
-    }
-    
-    if (lxt_resolve_container(&tmp, container, template) != 0) {
-        return -1;
-    }
-    
-    cursor->offset += tmp.offset;
-    
-    return 0;
-}
-
-static
-int32_t
-lxt_resolve_generator(struct lxt_cursor * const cursor,
-                      struct lxt_generator const * const generator,
-                      struct lxt_template const * const template)
-{
-    size_t n = 0; // constraint to stay within substring length
-    
-    char const * p = generator->resolution.start;
-    
-    struct lxt_token variable;
-    
-    bool token_started = false;
-    bool token_ended = false;
-    
-    while (*p && n != generator->resolution.length) {
-        if (strncmp(p, "@", 1) == 0) {
-            if (token_started) {
-                return -1;
-            }
-            
-            if (isspace(*(p + 1))) {
-                // immediately followed by whitespace
-                return -1;
-            }
-            
-            // skip this character
-            p++;
-            n++;
-            
-            variable.start = p;
-            variable.length = 0;
-            
-            token_started = true;
-        }
-        
-        if (token_started && isspace(*p)) {
-            token_started = false;
-            token_ended = true;
-        }
-        
-        if (token_started && !token_ended) {
-            variable.length += 1;
-        }
-        
-        if (token_ended) {
-            token_ended = false;
-            
-            if (lxt_resolve_variable(cursor, &variable, template) != 0) {
-                return -1;
-            }
-        }
-        
-        if (!token_started && !token_ended) {
-            if (cursor->offset >= cursor->length) {
-                return -1;
-            }
-            
-            cursor->buffer[cursor->offset] = *p;
-            cursor->offset += 1;
-        }
-        
-        p++;
-        n++;
-    }
-    
-    if (token_started && !token_ended) {
-        if (lxt_resolve_variable(cursor, &variable, template) != 0) {
-            return -1;
-        }
-    }
-    
-    return 0;
+    return (x & 0xffffffffUL);
 }
 
 static
@@ -420,7 +236,7 @@ lxt_parse(struct lxt_template * const result, char const * pattern)
                 container = &result->containers[result->container_count++];
                 container->entry = token;
             } break;
-            
+                
             case LXT_KIND_CONTAINER_ENTRY: {
                 if (container == NULL) {
                     return -1;
@@ -460,3 +276,187 @@ lxt_parse(struct lxt_template * const result, char const * pattern)
     return 0;
 }
 
+static
+int32_t
+lxt_resolve_generator(struct lxt_cursor * const cursor,
+                      struct lxt_generator const * const generator,
+                      struct lxt_template const * const template)
+{
+    size_t n = 0; // constraint to stay within substring length
+    
+    char const * p = generator->resolution.start;
+    
+    struct lxt_token variable;
+    
+    bool token_started = false;
+    bool token_ended = false;
+    
+    while (*p && n != generator->resolution.length) {
+        if (strncmp(p, "@", 1) == 0) {
+            if (token_started) {
+                return -1;
+            }
+            
+            if (isspace(*(p + 1))) {
+                // immediately followed by whitespace
+                return -1;
+            }
+            
+            // skip this character
+            p++;
+            n++;
+            
+            variable.start = p;
+            variable.length = 0;
+            
+            token_started = true;
+        }
+        
+        if (token_started && isspace(*p)) {
+            token_started = false;
+            token_ended = true;
+        }
+        
+        if (token_started && !token_ended) {
+            variable.length += 1;
+        }
+        
+        if (token_ended) {
+            token_ended = false;
+            
+            if (lxt_resolve_variable(cursor, &variable, template) != 0) {
+                return -1;
+            }
+        }
+        
+        if (!token_started && !token_ended) {
+            if (cursor->offset >= cursor->length) {
+                return -1;
+            }
+            
+            cursor->buffer[cursor->offset] = *p;
+            cursor->offset += 1;
+        }
+        
+        p++;
+        n++;
+    }
+    
+    if (token_started && !token_ended) {
+        if (lxt_resolve_variable(cursor, &variable, template) != 0) {
+            return -1;
+        }
+    }
+    
+    return 0;
+}
+
+static
+int32_t
+lxt_resolve_variable(struct lxt_cursor * const cursor,
+                     struct lxt_token const * const variable,
+                     struct lxt_template const * const template)
+{
+    struct lxt_cursor tmp;
+    
+    tmp.buffer = cursor->buffer + cursor->offset;
+    tmp.length = cursor->length - cursor->offset;
+    tmp.offset = 0;
+    
+    struct lxt_generator const * const generator = lxt_find_generator(variable,
+                                                                      template);
+    
+    if (generator != NULL) {
+        if (lxt_resolve_generator(&tmp, generator, template) != 0) {
+            return -1;
+        }
+        
+        cursor->offset += tmp.offset;
+        
+        return 0;
+    }
+    
+    struct lxt_container const * const container = lxt_find_container(variable,
+                                                                      template);
+    
+    if (container == NULL) {
+        return -1;
+    }
+    
+    size_t const entry_index = lxt_rand32(template->seed) % container->entry_count;
+    
+    struct lxt_token const * entry = &container->entries[entry_index];
+    
+    if (lxt_append_entry(&tmp, entry) != 0) {
+        return -1;
+    }
+    
+    cursor->offset += tmp.offset;
+    
+    return 0;
+}
+
+static
+int32_t
+lxt_append_entry(struct lxt_cursor * const cursor,
+                 struct lxt_token const * entry)
+{
+    if (entry->length >= cursor->length - cursor->offset) {
+        return -1;
+    }
+    
+    cursor->offset += entry->length;
+    
+    memcpy(cursor->buffer, entry->start, entry->length);
+    
+    return 0;
+}
+
+static
+struct lxt_generator const *
+lxt_find_generator(struct lxt_token const * const token,
+                   struct lxt_template const * const template)
+{
+    for (size_t i = 0; i < template->generator_count; i++) {
+        struct lxt_generator const * const match = &template->generators[i];
+        
+        if (lxt_token_equals(token, match->entry.start, match->entry.length)) {
+            return match;
+        }
+    }
+    
+    return NULL;
+}
+
+static
+struct lxt_container const *
+lxt_find_container(struct lxt_token const * const token,
+                   struct lxt_template const * const template)
+{
+    for (size_t i = 0; i < template->container_count; i++) {
+        struct lxt_container const * const match = &template->containers[i];
+        
+        if (lxt_token_equals(token, match->entry.start, match->entry.length)) {
+            return match;
+        }
+    }
+    
+    return NULL;
+}
+
+static
+bool
+lxt_token_equals(struct lxt_token const * const token,
+                 char const * const name,
+                 size_t const length)
+{
+    if (token->length != length) {
+        return false;
+    }
+    
+    if (strncmp(token->start, name, token->length) != 0) {
+        return false;
+    }
+    
+    return true;
+}
